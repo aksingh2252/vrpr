f1a5d34290a5b4238cfa0c2e4e1a9d4dab8d537b
Rahul <rahulkumar397222@gmail.com>
2024-05-21 19:10:48 +0530

    add a secondary display frame

--- Src/main.py
+++ Src/main.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from validate import validate
 
 cap = cv.VideoCapture("../Videos/shona2.mp4")
+# cap = cv.VideoCapture(0)
 
 if not cap.isOpened():
     print("No source found!!")
@@ -20,12 +21,22 @@ tracker = Sort()
 currentId = 0
 currentTime = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 fileName = f"output/vrpr_{currentTime}.csv"
+frameSize = 50
 
 while True:
     success, img = cap.read()
     if success:
-        results = model(source=img, show=False, stream=False)
+        img_height, img_width, _ = img.shape
+        new_img_height = int(img_height * frameSize / 100)
+        new_img_width = int(img_width * frameSize / 100)
+        dim = (new_img_width, new_img_height)
+        resized_img = cv.resize(img, dim)
+
+
         detections = np.empty((0, 4))
+        results = model(source=img, show=False, stream=True)
+        result_resized = model(source=resized_img, show=False, stream=True)
+
         for result in results:
             boxes = result.boxes
             for box in boxes:
@@ -43,8 +54,11 @@ while True:
 
             roi = img[y1:y2, x1:x2]
 
-            cv.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv.putText(img, f"{obj_id}", (x1, y1 - 10), 0, 1, (0, 0, 255), 1)
+
+            # cv.putText(img, f"{obj_id}", (x1, y1 - 10), 0, 1, (0, 0, 255), 1)
+
+            # cv.imshow("plate", img)
+            # cv.waitKey(1)
 
             gray_roi = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
             text = reader.readtext(gray_roi)
@@ -58,9 +72,16 @@ while True:
 
             except IndexError:
                 print("Plate number not found.")
-   
-            # cv.imshow("roi", roi)
-            # cv.imshow("main", img)
+
+        for result in result_resized:
+            boxes = result.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                cv.rectangle(resized_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv.imshow("main", resized_img)
+                cv.waitKey(1)
 
     else:
         print("End")
