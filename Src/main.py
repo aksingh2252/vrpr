dc448cbf42e3ec51127152d1424dc825fabd51b1
Rahul <rahulkumar397222@gmail.com>
2024-05-21 09:10:46 +0530

    modify main.py

--- Src/main.py
+++ Src/main.py
@@ -1,14 +1,9 @@
-import json
-
-import numpy as np
 from ultralytics import YOLO
 import cv2 as cv
 import easyocr
 from config import api_keys
 from Utils.sort import *
 from datetime import datetime
-
-
 
 from validate import validate
 
@@ -21,17 +16,15 @@ if not cap.isOpened():
 api_key = api_keys[0]
 reader = easyocr.Reader(['en'])
 model = YOLO("../Train/runs/detect/train/weights/best.pt")
-output_file = "output.txt"
 tracker = Sort()
 currentId = 0
-
 currentTime = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-fileName = f"output/vrpr_{currentTime}.txt"
+fileName = f"output/vrpr_{currentTime}.csv"
 
 while True:
     success, img = cap.read()
     if success:
-        results = model(source=img, show=False, stream=True)
+        results = model(source=img, show=False, stream=False)
         detections = np.empty((0, 4))
         for result in results:
             boxes = result.boxes
@@ -45,23 +38,23 @@ while True:
         resultTracker = tracker.update(detections)
 
         for result in resultTracker:
-            x1, y1, x2, y2, id = result
-            x1, y1, x2, y2, id = int(x1), int(y1), int(x2), int(y2), int(id)
+            x1, y1, x2, y2, obj_id = result
+            x1, y1, x2, y2, obj_id = int(x1), int(y1), int(x2), int(y2), int(obj_id)
 
             roi = img[y1:y2, x1:x2]
 
             cv.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv.putText(img, f"{id}", (x1, y1 - 10), 0, 1, (0, 0, 255), 1)
+            cv.putText(img, f"{obj_id}", (x1, y1 - 10), 0, 1, (0, 0, 255), 1)
 
             gray_roi = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
             text = reader.readtext(gray_roi)
 
             try:
                 plate_number = text[0][1]
-                if validate(plate_number) and id > currentId:
+                if validate(plate_number) and obj_id > currentId:
                     with open(fileName, 'w') as file:
-                        file.write(f"{id}, {plate_number}\n")
-                        currentId = id
+                        file.write(f"{obj_id}, {plate_number}\n")
+                        currentId = obj_id
 
             except IndexError:
                 print("Plate number not found.")
