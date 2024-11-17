5ceed3691444fc7d8b05700211768859e8336cbd
Rahul <rahulkumar397222@gmail.com>
2024-11-17 12:20:58 +0530

    finalize for easyocr

--- Src/validate.py
+++ Src/validate.py
@@ -1,4 +1,5 @@
 import re
+from colorama import Fore, Style
 from info import info
 
 
@@ -6,7 +7,8 @@ def validate(plate_number):
     pattern = r"^[A-Za-z]{2}[ -]?[0-9]{2}[ -]?[A-Za-z]{1,2}[ -]?[0-9]{4}$"
     match = re.match(pattern, plate_number)
     if match:
-        print("Valid plate number:", plate_number)
+        print(Fore.GREEN + "Valid plate number:", plate_number)
+        print(Style.RESET_ALL)
         # reg_info = info(plate_number, api_key=api_key)
         # print(reg_info)
 
